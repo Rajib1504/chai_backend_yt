d048adc3964f25f489ed0edfc59a356693b92b3a
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2026-05-01 10:07:21 +0530

    fix: correct typo in subscription model field definitions

--- src/model/subscription.models.js
+++ src/model/subscription.models.js
@@ -2,11 +2,11 @@ import mongoose, { Schema } from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema({
   subscriber: {
-    types: Schema.Types.ObjectId,
+    types: Schema.Types.ObjectId,// one who is subscribing 
     ref: "User",
   },
   channel: {
-    types: Schema.Types.ObjectId,
+    types: Schema.Types.ObjectId,// one whom "subscriber" is subscribing
     ref: "User",
   },
 });
